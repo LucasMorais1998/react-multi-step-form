e4f4c2ee70354b305322849afb7f612a69593d18
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-08-26 12:35:53 -0300

    import FormProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { FormProvider } from "./providers/FormProvider";
+
 import { Router } from "./router";
+
 import { GlobalStyle } from "./styles/global";
 
 const App = () => {
   return (
-    <>
+    <FormProvider>
       <GlobalStyle />
       <Router />
-    </>
+    </FormProvider>
   );
 };
 
