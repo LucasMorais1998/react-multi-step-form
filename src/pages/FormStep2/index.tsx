5bdfc66c63e66c8aef702130fb1935435ccda895
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-08-26 19:34:02 -0300

    add page/FormStep2

---
+++ src/pages/FormStep2/index.tsx
@@ -0,0 +1,99 @@
+import { useEffect } from "react";
+
+import { useForm } from "../../hooks/useForm";
+
+import { Link, useNavigate } from "react-router-dom";
+
+import { FormActions } from "../../enums/FormActions";
+
+import SelectOption, {
+  ISelectOptionsProps,
+} from "../../components/SelectOption";
+import Theme from "../../components/Theme";
+
+import { Container } from "./styles";
+
+type OptionsTypeOmited = Omit<ISelectOptionsProps, "selected" | "onClick">;
+
+interface OptionsType extends OptionsTypeOmited {
+  level: number;
+}
+
+const options: OptionsType[] = [
+  {
+    title: "Sou Junior",
+    description: "Programo há menos de 1 ano.",
+    icon: "👶",
+    level: 0,
+  },
+  {
+    title: "Sou Pleno",
+    description: "Programo há pelo menos 3 anos.",
+    icon: "🧑",
+    level: 1,
+  },
+  {
+    title: "Sou Sênior",
+    description: "Programo há mais de 5 anos",
+    icon: "👴",
+    level: 2,
+  },
+];
+
+const FormStep2 = () => {
+  const navigate = useNavigate();
+
+  const { state, dispatch } = useForm();
+
+  useEffect(() => {
+    if (state.name === "") navigate("/");
+
+    dispatch({
+      type: FormActions.setCurrentStep,
+      payload: 2,
+    });
+  }, []);
+
+  const handleNextStep = () => {
+    navigate("/step3");
+  };
+
+  const setLevel = (level: number) => {
+    dispatch({
+      type: FormActions.setLevel,
+      payload: level,
+    });
+  };
+
+  return (
+    <Theme>
+      <Container>
+        <p>Passo 2/3</p>
+        <h1>{state.name}, o que melhor descreve você?</h1>
+        <p>
+          Escolha a opção que melhor condiz com seu estado atual,
+          profissionalmente.
+        </p>
+        <hr />
+
+        {options.map((option, index) => (
+          <SelectOption
+            key={index}
+            title={option.title}
+            description={option.description}
+            icon={option.icon}
+            selected={Boolean(state.level === index)}
+            onClick={() => setLevel(index)}
+          />
+        ))}
+
+        <Link to="/" className="back-button">
+          Voltar
+        </Link>
+        <button onClick={handleNextStep}>Próximo</button>
+      </Container>
+    </Theme>
+  );
+};
+
+export default FormStep2;
---
+++ src/pages/FormStep2/styles.ts
@@ -0,0 +1,74 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  p {
+    font-size: 0.813rem;
+    color: var(--purple-100);
+    margin: 0.813rem 0;
+
+    &:last-of-type {
+      margin-bottom: 2rem;
+    }
+  }
+
+  h1 {
+    margin: 0;
+    padding: 0;
+    font-size: 1.625rem;
+  }
+
+  hr {
+    height: 1px;
+    border: 0;
+    background-color: var(--blue-300);
+    margin-bottom: 2rem;
+  }
+
+  label {
+    font-size: 0.813rem;
+    min-height: 400px;
+
+    input {
+      display: block;
+      margin-top: 0.438rem;
+      width: 100%;
+      padding: 1.25rem 0.625rem;
+      border: 2px solid var(--green-500);
+      border-radius: 0.625rem;
+      color: var(--white);
+      outline: none;
+      font-size: 0.938rem;
+      background-color: var(--blue-500);
+    }
+
+    span {
+      display: block;
+      color: var(--red);
+    }
+  }
+
+  button {
+    background-color: var(--green-500);
+    color: var(--white);
+    font-size: 0.875rem;
+    font-weight: bold;
+    padding: 1.25rem 2.5rem;
+    border: 0;
+    border-radius: 1.875rem;
+    margin-top: 1.875rem;
+    margin-bottom: 1.875rem;
+  }
+
+  .back-button {
+    font-size: 1rem;
+    text-decoration: none;
+    padding: 1.25rem 2.5rem;
+    color: var(--purple-100);
+    opacity: 0.6;
+    transition: opacity ease 0.2s;
+
+    &:hover {
+      opacity: 1;
+    }
+  }
+`;
