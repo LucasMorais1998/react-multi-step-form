0c707a7a20fa75e927c7eb9dcf07a6bbe25e5b9c
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-08-26 12:26:49 -0300

    feat hooks/useForm

---
+++ src/hooks/useForm.ts
@@ -0,0 +1,12 @@
+import { useContext } from "react";
+
+import { FormContext } from "../contexts/FormContext";
+
+export const useForm = () => {
+  const context = useContext(FormContext);
+
+  if (context === undefined)
+    throw new Error("useForm precisa ser usado dentro do FormProvider");
+
+  return context;
+};
