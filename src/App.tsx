21dbf63a20ef567ac9d4ed3a7acc5f6ec3bf335d
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-08-25 21:34:59 -0300

    feat router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Router } from "./router";
 import { GlobalStyle } from "./styles/global";
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
+      <Router />
     </>
   );
 };
