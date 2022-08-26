735ada5e9efea2f47bb0fc40500a3bdfd6f22ee6
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-08-26 12:21:14 -0300

    feat reducers/FormReducer

---
+++ src/reducers/FormReducer.ts
@@ -0,0 +1,26 @@
+import { FormActions } from "../enums/FormActions";
+
+import { Action } from "../types/Action";
+import { State } from "../types/State";
+
+export const FormReducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case FormActions.setCurrentStep:
+      return { ...state, currentStep: action.payload };
+
+    case FormActions.setName:
+      return { ...state, name: action.payload };
+
+    case FormActions.setLevel:
+      return { ...state, level: action.payload };
+
+    case FormActions.setEmail:
+      return { ...state, email: action.payload };
+
+    case FormActions.setGithub:
+      return { ...state, github: action.payload };
+
+    default:
+      return state;
+  }
+};
