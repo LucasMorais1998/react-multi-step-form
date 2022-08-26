b3d266d38f7d830b20c07474dd59677d3746d684
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-08-26 12:25:06 -0300

    feat providers\FormProvider

---
+++ src/providers/FormProvider.tsx
@@ -0,0 +1,30 @@
+import { ReactNode, useReducer } from "react";
+
+import { FormContext } from "../contexts/FormContext";
+
+import { FormReducer } from "../reducers/FormReducer";
+
+import { State } from "../types/State";
+
+interface IFormProviderProps {
+  children: ReactNode;
+}
+
+const initialData: State = {
+  currentStep: 0,
+  name: "",
+  level: 0,
+  email: "",
+  github: "",
+};
+
+export const FormProvider = ({ children }: IFormProviderProps) => {
+  const [state, dispatch] = useReducer(FormReducer, initialData);
+  const value = { state, dispatch };
+
+  return (
+    <FormContext.Provider value={value}>
+      {children}
+    </FormContext.Provider>
+  );
+};
