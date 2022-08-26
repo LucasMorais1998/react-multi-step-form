4a281df37ad742ce27f9cfc3913ec8f05a922d95
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-08-26 12:53:08 -0300

    add component/Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import { Container } from "./styles";
+
+const Header = () => {
+  return (
+    <Container>
+      <h1>Cadastro de Desenvolvedor</h1>
+      <p>
+        Faça seu cadastro na lista e participe dos nossos processos seletivos.
+      </p>
+    </Container>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+  padding: 1.563rem 0;
+  border-bottom: 1px solid var(--blue-300);
+
+  h1 {
+    margin: 0;
+    padding: 0;
+    font-size: 1.75rem;
+  }
+
+  p {
+    font-size: 0.875;
+    color: var(--purple-100);
+  }
+`;
